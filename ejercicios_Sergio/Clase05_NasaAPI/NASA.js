29470bc37fcfbdd482dd5abd4b95f57820b05307
Ortiz Hernández <SergioOrt@youforce.net>
2018-09-27 15:54:47 +0200

    limit added

--- ejercicios_Sergio/Clase05_NasaAPI/NASA.js
+++ ejercicios_Sergio/Clase05_NasaAPI/NASA.js
@@ -1,24 +1,34 @@
 const token = "cVXCwImHNJ2mCFkIirKZk4e6AruCMg6ldgw0yOIl";
+var counter = 0;
 
 async function NasaRequest(currentSun, limit, frecuency) {
 
   let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${currentSun}&api_key=${token}`
   let request = await fetch(url);
   let data = await request.json();
+  counter++;
 
   try {
 
     console.log(`request started for: ${request.url}`);
 
-    if (data.photos.length > 0) {
-      console.log(data.photos);
+    if(!limit || counter < limit) {
+
+      console.log(`counter: ${counter}, limit: ${limit}`);
+
+      if (data.photos.length) {
+        console.log(data.photos);
+      }
+      else {
+        console.log(`Delay for next request: ${frecuency}ms`);
+
+        setTimeout(() => {
+          NasaRequest(currentSun - 1, limit, frecuency);
+        }, frecuency);
+      }
     }
     else {
-      console.log(`Delay for next request: ${frecuency}ms`)
-
-      setTimeout(() => {
-        NasaRequest(currentSun - 1, limit, frecuency);
-      }, frecuency);
+      console.log(`Requests limit reached ${limit}`);
     }
   }
   catch (error) {
@@ -35,7 +45,7 @@ function init() {
     - limit requests: false
     - frecuency: 1000ms
   */
-  const currentValue = NasaRequest(2080, false, 2000);
+  const currentValue = NasaRequest(2178, 3, 1000);
   console.log("currentValue:", currentValue);
 }
 init();
