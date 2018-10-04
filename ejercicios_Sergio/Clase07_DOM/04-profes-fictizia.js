397739a2899edb18caa3cb43dc1efcdd090bc188
Sergio Ortiz <sergio.ortiz@raet.com>
2018-10-04 19:22:14 +0200

    doc added

--- ejercicios_Sergio/Clase07_DOM/04-profes-fictizia.js
+++ ejercicios_Sergio/Clase07_DOM/04-profes-fictizia.js
@@ -1,25 +1,22 @@
 /**
  * Nos creamos un array de objetos con la información, links y fotografías de l@s profes de Fictizia
+ * https://www.fictizia.com/profesorado
  */
 
 var teachersInfo = []
 
 var teachersList = Array.prototype.slice.call(document.querySelectorAll('article.microCard'));
 
-
-
 teachersList.forEach((item, index) => {
 
-    var teachersLinks = teachersList[index].querySelectorAll('.microBtns li');
-
+    var teachersLinks = Array.prototype.slice.call(teachersList[index].querySelectorAll('.microBtns li'));
 
     teachersInfo.push({
         name: teachersList[index].querySelector('h3').innerText,
         info: teachersList[index].querySelector('p').innerText,
         links: [
             teachersLinks.forEach((item, index) => {
-                console.log(item);
-                teachersLinks[index].querySelectorAll('a').href;
+                teachersLinks[index].querySelectorAll('a').innerHTML;
             })
         ],
         picture: teachersList[index].querySelector('.withMedia img').src
