6326eef3b7ed749dec4b5216b6022e24068e7de2
Sergio Ortiz <sergio.ortiz@raet.com>
2018-10-14 12:12:26 +0200

    Ejercicio cuadros electricos Gijon console.info borrado en tiempo Madrid

--- ejercicios_Sergio/Clase09_AJAX/03_cuadros_electricos_Gijon/cuadros_electricos_Gijon.js
+++ ejercicios_Sergio/Clase09_AJAX/03_cuadros_electricos_Gijon/cuadros_electricos_Gijon.js
@@ -1,7 +1,28 @@
 /*
     Jugando con datos abiertos (http://datos.gob.es/) saquemos los detalles de todos los cuadros eléctricos de Gijón por consola.
 */
-(function(){
+
+// http://opendata.gijon.es/descargar.php?id=163&tipo=JSON
+// http://opendata.gijon.es/descargar.php?id=163&tipo=HTML
 
 
-});
+
+    function peticionAjax(url) {
+        var xmlHttp = new XMLHttpRequest();
+        
+        xmlHttp.onreadystatechange = function() {
+
+            if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+                console.info(JSON.parse(xmlHttp.responseText));
+                weather = JSON.parse(xmlHttp.responseText);
+
+            } else if (xmlHttp.readyState === 4 && xmlHttp.status === 404) {
+                console.error("ERROR! 404");
+                console.info(JSON.parse(xmlHttp.responseText));
+            }
+        };
+        xmlHttp.open("GET", url, true);
+        xmlHttp.send();
+    }
+
+peticionAjax('http://opendata.gijon.es/descargar.php?id=163&tipo=JSON');
